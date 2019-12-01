5c675da5d31c01a034ff5c6d90c06aac5a073732
Mauricio Hernan Cabrera <mauriciohernancabrera@gmail.com>
2019-12-01 20:33:46 -0300

    deploy

---
+++ precache-manifest.ab536c25fcca94d38b723b438d5d01e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6e048064e36fa343b0a456cd33d34a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd39a4d03d1e0abde336",
+    "url": "/static/css/main.c2b9e2cd.chunk.css"
+  },
+  {
+    "revision": "07d9872448dbbecce59a",
+    "url": "/static/js/2.9ffc511a.chunk.js"
+  },
+  {
+    "revision": "bd39a4d03d1e0abde336",
+    "url": "/static/js/main.5a1c5ad4.chunk.js"
+  },
+  {
+    "revision": "c4e0e959417b14012c0c",
+    "url": "/static/js/runtime-main.9590971c.js"
+  }
+]);
